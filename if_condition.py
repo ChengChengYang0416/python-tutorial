5bd39d37aafc2095700b4cffee1650f73570a784
ChengChengYang1997 <kb240608@gmail.com>
2020-07-15 22:24:11 +0800

    added if_condition.py

---
+++ if_condition.py
@@ -0,0 +1,10 @@
+#-------------- 1. if --------------#
+num = int(input("type a number : "))
+
+if num > 20:
+    print("the input is bigger than 20.")
+elif num > 10:
+    print("the input is smaller than 20 and bigger than 10.")
+else:
+    print("the input is smaller or equal to 10.")
+#-------------- end of if --------------#
