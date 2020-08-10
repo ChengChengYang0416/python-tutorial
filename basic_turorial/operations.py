e4cc20899c612203c285e41e01f5c38123799550
ChengChengYang1997 <kb240608@gmail.com>
2020-08-10 21:46:09 +0800

    Rearrange files

---
+++ basic_turorial/operations.py
@@ -0,0 +1,32 @@
+#-------------- 1. operations in numbers --------------#
+# division
+divi_1 = 7/6
+print(divi_1)
+divi_2 = 7//6
+print(divi_2)
+
+# power
+power = 3**4
+print(power)
+
+# modulo
+modu = 13%3
+print(modu)
+#-------------- end of operations in numbers --------------#
+
+
+#-------------- 2. operations in strings --------------#
+s = "I" + " am" + " a" + " string."
+print(s)
+s1 = "I" " am" " a" " string."
+print(s1)
+s2 = "Hello"*3
+print(s2)
+
+print("the first element of s2 is :"),
+print(s2[0])
+print("the second element of s2 is :"),
+print(s2[1])
+print("the second element to the fifth element of s2 is :"),
+print(s2[1:5])
+#-------------- end of operations in strings --------------#
