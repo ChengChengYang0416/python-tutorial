78da448c0ea028e6517984fc3506b41d5ea926e6
Cheng-Cheng Yang <chengchengyang0416@gmail.com>
2022-02-14 20:12:56 +0800

    upload tutorial repo for Tkinter

---
+++ gui_Tkinter/main.py
@@ -0,0 +1,11 @@
+# use Tkinter for python2, tkinter for python3
+import Tkinter as tk
+
+# build main window
+Win = tk.Tk()
+
+# set title
+Win.title( "Tkinter-tutorial" )
+
+# let Tkinter start running the application
+Win.mainloop()
