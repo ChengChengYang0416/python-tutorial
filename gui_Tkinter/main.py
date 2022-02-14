1747d46c1de03bb6bfe9ea2bcd5139c42d045a88
Cheng-Cheng Yang <chengchengyang0416@gmail.com>
2022-02-14 20:41:09 +0800

    example for geometry, color, transparency, position of the window

--- gui_Tkinter/main.py
+++ gui_Tkinter/main.py
@@ -7,5 +7,16 @@ Win = tk.Tk()
 # set title
 Win.title( "Tkinter-tutorial" )
 
+# set geometry of window ( width x height )
+Win.geometry( "400x300" )
+Win.minsize( width = 400, height = 300 )
+Win.maxsize( width = 800, height = 600 )
+Win.resizable( True, True )
+
+# color, transparency, pin on top
+Win.config( background = "#C0C0C0" )
+Win.attributes( "-alpha", 0.65 )
+Win.attributes( "-topmost", True )
+
 # let Tkinter start running the application
 Win.mainloop()
