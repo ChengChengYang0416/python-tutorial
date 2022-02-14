0d452a685464b4350c70cb5e6227259eb57b7555
Cheng-Cheng Yang <chengchengyang0416@gmail.com>
2022-02-14 21:24:55 +0800

    add comments for basic setting and button example

--- gui_Tkinter/main.py
+++ gui_Tkinter/main.py
@@ -5,6 +5,7 @@ import ButtonFcn.ButtonFcn as ButtonFcn
 # build main window
 Win = tk.Tk()
 
+#------------------- basic setting -------------------#
 # set title
 Win.title( "Tkinter-tutorial" )
 
@@ -18,17 +19,20 @@ Win.resizable( True, True )
 Win.config( background = "#C0C0C0" )
 Win.attributes( "-alpha", 0.65 )
 Win.attributes( "-topmost", True )
+#------------------- end of basic setting -------------------#
 
-# button
+#------------------- button example -------------------#
+# basic button with pixel size
 Pixel = tk.PhotoImage( width = 1, height = 1 )
 Btn = tk.Button( text = "Button", image = Pixel, background = "#FFFFFF", width = 40, height = 10, compound = "c" )
 Btn.pack()
 
-# insert image to button
+# insert image to button and show the text simultaneously
 Img = tk.PhotoImage( file = "Button.png" )
 ImgResize = Img.subsample( 20, 20 )
 BtnImage = tk.Button( text = "Click me !", image = ImgResize, compound = "left", command = ButtonFcn.PrintHello )
 BtnImage.pack()
+#------------------- end of button example -------------------#
 
 # let Tkinter start running the application
 Win.mainloop()
